0847cf3ba083f8b473497ee8773fc9ffd90c5aaa
Gabriellpa <gabriellpantunes@gmail.com>
2021-02-02 11:04:29 -0300

    :sparkles: Done

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,66 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('You do not have enough balance');
+    }
+
+    const transactionCategory = await this.getCategory(category);
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: transactionCategory,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
+  }
+
+  private async getCategory(category: string): Promise<Category> {
+    const categoryRepository = getRepository(Category);
+
+    const transactionCategory = await categoryRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    if (!transactionCategory) {
+      const transactionCreateCategory = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(transactionCreateCategory);
+
+      return transactionCreateCategory;
+    }
+
+    return transactionCategory;
   }
 }
 
